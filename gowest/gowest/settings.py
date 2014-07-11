39fbf3540667c3e6cd68d5e607a4034244a2d3eb
Adrián Deccico <deccico@gmail.com>
2014-07-12 09:43:26 +1000

    adding main app 'go'

--- gowest/gowest/settings.py
+++ gowest/gowest/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gpqm(^3@zb9m!dc*fb-w$ea0mcw1qtz89bay0dlftvdjvu*lg!'
+SECRET_KEY = 'ugkja!^57&39cp&h6hxi3g^7*dur&lma-f3b=y20+l&$ca_1!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
