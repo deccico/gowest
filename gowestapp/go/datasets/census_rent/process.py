7ea1545bc74044f2b4ea1cb47c4d6a18c5971494
Cong <congusbongus@gmail.com>
2014-07-12 13:57:56 +1000

    Census data is per LGA

--- gowestapp/go/datasets/census_rent/process.py
+++ gowestapp/go/datasets/census_rent/process.py
@@ -1,5 +1,5 @@
 '''
-Process raw data into dictionary: {state => {suburb => {weekly rent bin => count}}}
+Process raw data into dictionary: {state => {lga => {weekly rent bin => count}}}
 e.g. {'New South Wales' => {'North Sydney' => {'$450-$549' => 2790}}}
 '''
 import csv
@@ -14,23 +14,23 @@ def process():
             continue
         #"Local Government Areas (2011)","New South Wales","Albury (C)","Annual","2011","$0-$74","Real estate agent",11,
         state = row[1]
-        suburb = row[2][:row[2].rfind(' ')]    # ignore last token (C)
+        lga = row[2][:row[2].rfind(' ')]    # ignore last token (C)
         rentbin = row[5]
         number = int(row[7])
-        addrow(results, state, suburb, rentbin, number)
+        addrow(results, state, lga, rentbin, number)
     return results
 
 def getsomething():
     return "something"
 
-def addrow(results, state, suburb, rentbin, number):
+def addrow(results, state, lga, rentbin, number):
     if state not in results:
         results[state] = {}
-    if suburb not in results[state]:
-        results[state][suburb] = {}
-    if rentbin not in results[state][suburb]:
-        results[state][suburb][rentbin] = 0
-    results[state][suburb][rentbin] += number
+    if lga not in results[state]:
+        results[state][lga] = {}
+    if rentbin not in results[state][lga]:
+        results[state][lga][rentbin] = 0
+    results[state][lga][rentbin] += number
 
 if __name__ == '__main__':
     print process()
--- gowestapp/go/datasets/census_rent/readme.txt
+++ gowestapp/go/datasets/census_rent/readme.txt
@@ -3,4 +3,4 @@ http://stat.abs.gov.au/
 B34 Rent (weekly) by Landlord Type (LGA)
 
 Desc:
-Binned weekly rent in 2011, categorised by: state, suburb (LGA), weekly rent (bin), landlord type
+Binned weekly rent in 2011, categorised by: state, LGA, weekly rent (bin), landlord type
