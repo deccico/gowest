da428fbf4d92e72308c469520b91d3fd17319fc3
Cong <congusbongus@gmail.com>
2014-07-12 16:48:49 +1000

    Add western sydney LGAs and use in mapping

--- gowestapp/go/views.py
+++ gowestapp/go/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from go.datasets.lga_suburb_list import process
 
 def index(request):
+    westernSydneyLGAs = ['Auburn', 'Bankstown', 'Blacktown', 'Blue Mountains', 'Camden', 'Campbelltown', 'Fairfield', 'Hawkesbury', 'Hills Shire', 'Holroyd', 'Liverpool', 'Parramatta', 'Penrith', 'Wollondilly']
     context = {}
     suburbsToLGA, lgaToRegion = process.process()
     try:
@@ -10,19 +11,28 @@ def index(request):
         context['compare'] = ' '.join(w.capitalize() for w in compareu.encode('utf8').strip().split())    # trim and title capitalise
     except UnicodeDecodeError:
         context['compare'] = ''
-    context['info'] = getcompareinfo(context['compare'], suburbsToLGA, lgaToRegion)
+    context['info'] = getcompareinfo(context['compare'], suburbsToLGA, lgaToRegion, westernSydneyLGAs)
     return render(request, 'go/index.html', context)
 
-def getcompareinfo(compare, suburbToLGA, lgaToRegion):
+def getcompareinfo(compare, suburbToLGA, lgaToRegion, westernSydneyLGAs):
     if compare == '':
         return ''
 
     out = compare + ' is a '
 
     type = None
+    lgas = []
     lgaStr = ''
     region = ''
-    if compare in suburbToLGA:
+    if compare in lgaToRegion and compare in suburbToLGA:
+        type = 'suburb and local government area'
+        lgas = [compare]
+        region = lgaToRegion[compare]
+    elif compare in lgaToRegion:
+        type = 'local government area'
+        lgas = [compare]
+        region = lgaToRegion[compare]
+    elif compare in suburbToLGA:
         type = 'suburb'
         lgas = suburbToLGA[compare]
         lgaStr = ' in the local government area'
@@ -32,12 +42,12 @@ def getcompareinfo(compare, suburbToLGA, lgaToRegion):
             lgaStr += 's of ' + ', '.join(lgas[:-1]) + ' and ' + lgas[-1]
         lgaStr += ','
         region = lgaToRegion[lgas[0]]
-    elif compare in lgaToRegion:
-        type = 'local government area'
-        region = lgaToRegion[compare]
     if type is None:
         return ''
 
     out += type + lgaStr + ' in the ' + region + ' region.'
+
+    if len(lgas) > 0 and lgas[0] in westernSydneyLGAs:
+        out += ' It is part of Western Sydney!'
 
     return out
