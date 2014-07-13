b1e5b95b63ac6bc77c9c9e1253f6b5d863455dcc
Adrián Deccico <deccico@gmail.com>
2014-07-13 12:21:44 +1000

    fixing home link

--- gowestapp/gowest/urls.py
+++ gowestapp/gowest/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index.html/', include('go.urls')),
     url(r'', include('go.urls')),
 )
