bbf5f6ffd9d7bd17e23586efdb339bd08ab60285
Adrián Deccico <adrian@darumatic.com>
2021-09-04 12:59:09 +1000

    Update settings.py

--- gowestapp/gowest/settings.py
+++ gowestapp/gowest/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ugkja!^57&39cp&h6hxi3g^7*dur&lma-f3b=y20+l&$ca_1!='
+SECRET_KEY = 'your_secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
